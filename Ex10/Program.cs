28c35a16ef0212744c0f1f7d17465c673604d710
Ekaterina <katya.kinal@gmail.com>
2022-08-09 12:54:47 +0300

    Ex10

---
+++ Ex10/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите трехзначеное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int secondDigit = (number % 100) / 10;
+Console.WriteLine($"Вторая цифра числа {number} - {secondDigit}");
