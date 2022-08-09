e1283973f7a740662234191065bc90c014895927
Ekaterina <katya.kinal@gmail.com>
2022-08-09 13:27:41 +0300

    Ex13

---
+++ Ex13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+string number = Console.ReadLine();
+int length = number.Length;
+
+if(length >= 3)
+{
+    Console.WriteLine(number[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
