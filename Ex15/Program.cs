257f312816fbe525fbd06912daeb5192b08f76de
Ekaterina <katya.kinal@gmail.com>
2022-08-09 13:36:18 +0300

    Ex15

---
+++ Ex15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число, обозначающее день недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number == 6 ^ number == 7)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
